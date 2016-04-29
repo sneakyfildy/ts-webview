ccf86f06e3ab383cafc0122ffaf840989304f188
sneakyfildy <feeldeeng@gmail.com>
2016-04-29 17:33:32 +0400

    refactoring

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
 /* global require */
 (function(){
-    var test = require('./testmod');
-    var User = require('./server/User');
-    var fs = require('fs');
+    var User = require("./modules/User");
+    var fs = require("fs");
 
-    var express = require('express');
+    var express = require("express");
     var app = express();
-    var http = require('http').Server(app);
-    var io = require('socket.io')(http);
-    var util = require('util');
-    var exec = require('child_process').exec;
+    var http = require("http").Server(app);
+    var io = require("socket.io")(http);
+    var util = require("util");
+    var exec = require("child_process").exec;
     function puts(res, error, stdout, stderr) {
-        if (stdout === '' && !stderr) {
-            stdout = 'nothing';
+        if (stdout === "" && !stderr) {
+            stdout = "nothing";
         }
         if (error || stderr){
             console.log(error, stderr);
         }
-        io.emit('backend message', 'submit');
+        io.emit("backend message", "submit");
         res.end(stdout);
     }
 
     var TS_FILEPATH_EXEC, TS_FILEPATH_TEMPLATE;
-    TS_FILEPATH_TEMPLATE = 'iponweb/admin/timesheet/{0}';
-    TS_FILEPATH_EXEC = makeTsPath('`whoami`');
+    TS_FILEPATH_TEMPLATE = "/home/{0}/iponweb/admin/timesheet/{0}";
+    TS_FILEPATH_EXEC = makeTsPath("`whoami`");
     function makeTsPath(userName){
-        return TS_FILEPATH_TEMPLATE.replace('{0}', userName || User.name);
+        return TS_FILEPATH_TEMPLATE.replace(/\{0\}/g, userName || User.name);
     }
     User.updateName();
 
-    app.set('view engine', 'jade');
-    app.use(express.static('static'));
+    app.set("view engine", "jade");
+    app.use(express.static("static"));
 
-    app.get('/read', function (req, res) {
+    app.get("/read", function (req, res) {
         fs.readFile(makeTsPath(), function (err, data) {
             if (err) {
                 return console.log(err);
@@ -41,19 +40,19 @@
         });
     });
 
-    app.get('/submit', function (req, res) {
-        exec("cd ~ \n cvs -d :ext:`whoami`@www.iponweb.net:/var/cvs ci -m \"updated\" " + TS_FILEPATH_EXEC, puts.bind(this, res));
+    app.get("/submit", function (req, res) {
+        exec("cvs -d :ext:`whoami`@www.iponweb.net:/var/cvs ci -m \"updated\" " + TS_FILEPATH_EXEC, puts.bind(this, res));
     });
-    app.get('/user', function (request, response) {
+    app.get("/user", function (request, response) {
         User.getNameByRemoteRequest(response);
     });
-    app.post('/add', function (req, res) {
-        var data = '';
-        req.on('data', function (chunk) {
+    app.post("/add", function (req, res) {
+        var data = "";
+        req.on("data", function (chunk) {
             data += chunk;
         })
-        .on('end', function () {
-            fs.appendFile(makeTsPath(), JSON.parse(data).data + '\n', function(){
+        .on("end", function () {
+            fs.appendFile(makeTsPath(), JSON.parse(data).data + "\n", function(){
                 fs.readFile(makeTsPath(), function (err, data) {
                     if (err) {
                         return console.log(err);
@@ -65,21 +64,21 @@
         });
 
     });
-    app.get('/', function (req, res) {
-        res.render('index', {title: 'TS Ext WebView', message: 'Hello there!'});
+    app.get("/", function (req, res) {
+        res.render("index", {title: "TS Ext WebView", message: "Hello there!"});
     });
 
-    io.on('connection', function (socket) {
-        console.log('a user connected');
-        socket.on('disconnect', function () {
-            console.log('user disconnected');
+    io.on("connection", function (socket) {
+        console.log("a user connected");
+        socket.on("disconnect", function () {
+            console.log("user disconnected");
         });
-        socket.on('chat message', function (msg) {
-            console.log('message: ' + msg);
+        socket.on("chat message", function (msg) {
+            console.log("message: " + msg);
         });
     });
 
     http.listen(5555, function () {
-        console.log('Example app listening on port 5555!');
+        console.log("Example app listening on port 5555!");
     });
 })();
